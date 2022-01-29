d25190321c502b30f72aabd8a7a4d512e0d6d443
SHOCK <beponuka666@gmail.com>
2022-01-29 20:47:48 +0500

    Add Lesson_1_5_if_else

---
+++ Lesson_1_5_if_else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string nameuser = Console.ReadLine();
+if (nameuser.ToLower() == "миша") 
+{
+    Console.WriteLine("Привет пидор!");
+}
+else
+{
+    Console.Write("Привет! ");
+    Console.Write(nameuser);
+}
